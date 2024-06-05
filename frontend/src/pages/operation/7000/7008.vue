d19eedae9e5272737054b3a4860196eff54043f0
夂夂鱼 <dyzmj@qq.com>
2024-06-05 19:11:34 +0800

    增加7008部分计算逻辑

--- frontend/src/pages/operation/7000/7008.vue
+++ frontend/src/pages/operation/7000/7008.vue
@@ -1,16 +1,87 @@
-<template>
-    <div>
-        <p> This is 加药系统-PAM page </p>
-        <a-button type="primary" @click="backHome">返回首页</a-button>
+、<template>
+    <div class="calc_page" style="background-color: #EDEFF2;">
+        <a-row style="margin: 0 -6px">
+            <a-col style="padding: 14px 6px; margin-left: 0px;" :xl="8" :lg="24" :md="24" :sm="24" :xs="24">
+                <a-card :title="$t('b2')" style="margin-bottom: 24px" :bordered="false" :body-style="{ padding: 4 }">
+                        <a-form>
+                            <a-form-item :label="$t('b3')" style="margin-top: 10px" :labelCol="{ span: 8 }" :wrapperCol="{ span: 16 }">
+                                <a-input-group compact>
+                                        <a-input v-model="b3" style="width: 100%;" :disabled="false" rows="12" :placeholder="$t('b3')" :suffix="$t('b3_u')" />
+                                    </a-input-group>
+                            </a-form-item>
+                            <a-form-item :label="$t('b4')" style="margin-top: 10px" :labelCol="{ span: 8 }" :wrapperCol="{ span: 16 }">
+                                <a-input-group compact>
+                                        <a-input v-model="b4" style="width: 50%;" :disabled="false" rows="12" :placeholder="$t('b4')" :suffix="$t('b4_u')" />
+                                        <a-input v-model="b4_1" style="width: 50%;" :disabled="false" rows="12" :placeholder="$t('b4_1')" />
+                                    </a-input-group>
+                            </a-form-item>
+                            <a-form-item :label="$t('b5')" style="margin-top: 10px" :labelCol="{ span: 8 }" :wrapperCol="{ span: 16 }">
+                                <a-input-group compact>
+                                    <a-input v-model="b5" style="width: 50%;" :disabled="false" rows="12" :placeholder="$t('b5')" :suffix="$t('b5_u')" />
+                                    <a-input v-model="b5_1" style="width: 50%;" :disabled="false" rows="12" :placeholder="$t('b5_1')" :suffix="$t('b5_1_u')" />
+                                </a-input-group>
+                            </a-form-item>
+                        </a-form>
+                </a-card>
+                <a-card :title="$t('referenceImage')">
+                    <div class="baseQueryParam">
+                        <a-form>
+                            <a-form-item style="margin-top: 50px; margin-bottom: 60px">
+                                <img width="400px" src="@/assets/img/5000/5001_1.png" />
+                            </a-form-item>
+                        </a-form>
+                    </div>
+                </a-card>
+            </a-col>
+            <a-col style="padding: 14px 6px" :xl="8" :lg="24" :md="24" :sm="24" :xs="24">
+
+            </a-col>
+            <a-col style="padding: 14px 6px; margin-right: 0px;" :xl="8" :lg="24" :md="24" :sm="24" :xs="24">
+            </a-col>
+        </a-row>
     </div>
 </template>
 
 <script>
+
+import { mapState } from 'vuex'
+
 export default {
+    components: {},
+    i18n: require('./i18n_7008'),
+    data() {
+        return {
+            b3: '20000.00',
+            b4: '1',
+            b4_1: '1',
+        }
+    },
     methods: {
         backHome() {
             this.$router.push('/work')
+        },
+        exportQuantities() {
+            this.$message.warn(this.$t('exportQuantitiesNotOpen'))
+        },
+        exportComputeBook() {
+            this.$message.warn(this.$t('exportComputeBookNotOpen'))
         }
+        
     },
+    computed: {
+        ...mapState('setting', ['lang']),
+        b5() {
+            return (parseFloat(this.b3) / 24 + parseFloat(this.b4) / parseFloat(this.b4_1)).toFixed(2)
+        },
+        b5_1() {
+            return (parseFloat(this.b5) / 3600).toFixed(2)
+        },
+    }
 }
 </script>
+
+<style lang="less">
+.calc{
+    margin-bottom: 15px;
+}
+</style
