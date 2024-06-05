4a92a75ffcd916bf676b567c95d440b5746eaf31
夂夂鱼 <dyzmj@qq.com>
2024-06-05 13:37:46 +0800

    增加5006部分计算逻辑

--- frontend/src/pages/operation/5000/5006.vue
+++ frontend/src/pages/operation/5000/5006.vue
@@ -1,16 +1,96 @@
 <template>
-    <div>
-        <p> This is 平流式气浮池 page </p>
-        <a-button type="primary" @click="backHome">返回首页</a-button>
+    <div class="calc_page" style="background-color: #EDEFF2;">
+        <a-row style="margin: 0 -6px">
+            <a-col style="padding: 14px 6px; margin-left: 0px;" :xl="8" :lg="24" :md="24" :sm="24" :xs="24">
+                <a-card :title="$t('b2')" style="margin-bottom: 24px" :bordered="false" :body-style="{ padding: 4 }">
+                        <div class="baseQueryParam">
+                        <a-form>
+                            <a-form-item :label="$t('b3')" style="margin-top: 10px" :labelCol="{ span: 10 }" :wrapperCol="{ span: 14 }">
+                                <a-input-group compact>
+                                        <a-input v-model="b3" style="width: 50%;" :disabled="false" rows="12" :placeholder="$t('b3')" :suffix="$t('b3_u')" />
+                                    </a-input-group>
+                            </a-form-item>
+                            <a-form-item :label="$t('b4')" style="margin-top: 10px" :labelCol="{ span: 10 }" :wrapperCol="{ span: 14 }">
+                                    <a-input-group compact>
+                                        <a-input v-model="b4" rows="12" :placeholder="$t('b4')" :suffix="$t('b4_u')" />
+                                    </a-input-group>
+                            </a-form-item>
+                            <a-form-item :label="$t('b5')" style="margin-top: 10px" :labelCol="{ span: 10 }" :wrapperCol="{ span: 14 }">
+                                    <a-input-group compact>
+                                        <a-input v-model="b5" rows="12" :placeholder="$t('b5')" :suffix="$t('b5_u')" />
+                                    </a-input-group>
+                            </a-form-item>
+                            <a-form-item :label="$t('b6')" style="margin-top: 10px" :labelCol="{ span: 10 }" :wrapperCol="{ span: 14 }">
+                                    <a-input-group compact>
+                                        <a-input v-model="b6" rows="12" :placeholder="$t('b6')"/>
+                                    </a-input-group>
+                            </a-form-item>
+                            <a-form-item :label="$t('b7')" style="margin-top: 10px" :labelCol="{ span: 10 }" :wrapperCol="{ span: 14 }">
+                                    <a-input-group compact>
+                                        <a-input v-model="b7" rows="12" :placeholder="$t('b7')" :suffix="$t('b7_u')" />
+                                    </a-input-group>
+                            </a-form-item>
+                        </a-form>
+                    </div>
+                </a-card>
+                <a-card :title="$t('referenceImage')">
+                    <div class="baseQueryParam">
+                        <a-form>
+                            <a-form-item style="margin-top: 50px; margin-bottom: 60px">
+                                <img width="400px" src="@/assets/img/5000/5001_1.png" />
+                            </a-form-item>
+                        </a-form>
+                    </div>
+                </a-card>
+            </a-col>
+            <a-col style="padding: 14px 6px" :xl="8" :lg="24" :md="24" :sm="24" :xs="24">
+
+            </a-col>
+            <a-col style="padding: 14px 6px; margin-right: 0px;" :xl="8" :lg="24" :md="24" :sm="24" :xs="24">
+            </a-col>
+        </a-row>
     </div>
 </template>
 
 <script>
+
+import { mapState } from 'vuex'
+
 export default {
+    components: {},
+    i18n: require('./i18n_5006'),
+    data() {
+        return {
+            b3: '25000',
+            b4: '15',
+            b5: '1.5',
+            b6: '0.1',
+            b7: '2',
+        }
+    },
     methods: {
         backHome() {
             this.$router.push('/work')
+        },
+        exportQuantities() {
+            this.$message.warn(this.$t('exportQuantitiesNotOpen'))
+        },
+        exportComputeBook() {
+            this.$message.warn(this.$t('exportComputeBookNotOpen'))
         }
+        
     },
+    computed: {
+        ...mapState('setting', ['lang']),
+        b3_1() {
+            return (parseFloat(this.b3) / 3600).toFixed(4)
+        },
+    }
 }
 </script>
+
+<style lang="less">
+.calc{
+    margin-bottom: 15px;
+}
+</style>
