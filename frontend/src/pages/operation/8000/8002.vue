95acc4beb3e9957da3bfe85332af54b3184d9e63
夂夂鱼 <dyzmj@qq.com>
2024-06-05 20:28:03 +0800

    增加8002部分计算逻辑

--- frontend/src/pages/operation/8000/8002.vue
+++ frontend/src/pages/operation/8000/8002.vue
@@ -1,16 +1,129 @@
 <template>
-    <div>
-        <p> This is UF-GE page </p>
-        <a-button type="primary" @click="backHome">返回首页</a-button>
+    <div class="calc_page" style="background-color: #EDEFF2;">
+        <a-row style="margin: 0 -6px">
+            <a-col style="padding: 14px 6px; margin-left: 0px;" :xl="8" :lg="24" :md="24" :sm="24" :xs="24">
+                <a-card :title="$t('b4')" style="margin-bottom: 24px" :bordered="false" :body-style="{ padding: 4 }">
+                        <div class="baseQueryParam">
+                        <a-form>
+                            <a-form-item :label="$t('b6')" style="margin-top: 10px" :labelCol="{ span: 8 }" :wrapperCol="{ span: 16 }">
+                                <a-input-group compact>
+                                        <a-input v-model="b6" style="width: 100%;" :disabled="false" rows="12" :placeholder="$t('b6')" :suffix="$t('b6_u')" />
+                                    </a-input-group>
+                            </a-form-item>
+                            <a-form-item :label="$t('b7')" style="margin-top: 10px" :labelCol="{ span: 8 }" :wrapperCol="{ span: 16 }">
+                                <a-input-group compact>
+                                        <a-input v-model="b7" style="width: 100%;" :disabled="false" rows="12" :placeholder="$t('b7')" :suffix="$t('b7_u')" />
+                                    </a-input-group>
+                            </a-form-item>
+                            <a-form-item :label="$t('b8')" style="margin-top: 10px" :labelCol="{ span: 8 }" :wrapperCol="{ span: 16 }">
+                                <a-input-group compact>
+                                        <a-input v-model="b8" style="width: 100%;" :disabled="false" rows="12" :placeholder="$t('b8')" :suffix="$t('b8_u')" />
+                                    </a-input-group>
+                            </a-form-item>
+                            <a-form-item :label="$t('b9')" style="margin-top: 10px" :labelCol="{ span: 8 }" :wrapperCol="{ span: 16 }">
+                                <a-input-group compact>
+                                        <a-input v-model="b9" style="width: 100%;" :disabled="false" rows="12" :placeholder="$t('b9')" :suffix="$t('b9_u')" />
+                                    </a-input-group>
+                            </a-form-item>
+                            <a-form-item :label="$t('b10')" style="margin-top: 10px" :labelCol="{ span: 8 }" :wrapperCol="{ span: 16 }">
+                                <a-input-group compact>
+                                        <a-input v-model="b10" style="width: 100%;" :disabled="false" rows="12" :placeholder="$t('b10')" :suffix="$t('b10_u')" />
+                                    </a-input-group>
+                            </a-form-item>
+                            <a-form-item :label="$t('b11')" style="margin-top: 10px" :labelCol="{ span: 8 }" :wrapperCol="{ span: 16 }">
+                                <a-input-group compact>
+                                        <a-input v-model="b11" style="width: 100%;" :disabled="false" rows="12" :placeholder="$t('b11')" :suffix="$t('b11_u')" />
+                                    </a-input-group>
+                            </a-form-item>
+                            <a-form-item :label="$t('b12')" style="margin-top: 10px" :labelCol="{ span: 8 }" :wrapperCol="{ span: 16 }">
+                                <a-input-group compact>
+                                        <a-input v-model="b12" style="width: 100%;" :disabled="false" rows="12" :placeholder="$t('b12')" />
+                                    </a-input-group>
+                            </a-form-item>
+                            <a-form-item :label="$t('b13')" style="margin-top: 10px" :labelCol="{ span: 8 }" :wrapperCol="{ span: 16 }">
+                                <a-input-group compact>
+                                        <a-input v-model="b13" style="width: 100%;" :disabled="false" rows="12" :placeholder="$t('b13')" :suffix="$t('b13_u')" />
+                                    </a-input-group>
+                            </a-form-item>
+                            <a-form-item :label="$t('b14')" style="margin-top: 10px" :labelCol="{ span: 8 }" :wrapperCol="{ span: 16 }">
+                                <a-input-group compact>
+                                        <a-input v-model="b14" style="width: 100%;" :disabled="false" rows="12" :placeholder="$t('b14')" :suffix="$t('b14_u')" />
+                                    </a-input-group>
+                            </a-form-item>
+                            <a-form-item :label="$t('b15')" style="margin-top: 10px" :labelCol="{ span: 8 }" :wrapperCol="{ span: 16 }">
+                                <a-input-group compact>
+                                        <a-input v-model="b15" style="width: 100%;" :disabled="false" rows="12" :placeholder="$t('b15')" :suffix="$t('b15_u')" />
+                                    </a-input-group>
+                            </a-form-item>
+                            <a-form-item :label="$t('b16')" style="margin-top: 10px" :labelCol="{ span: 8 }" :wrapperCol="{ span: 16 }">
+                                <a-input-group compact>
+                                        <a-input v-model="b16" style="width: 100%;" :disabled="false" rows="12" :placeholder="$t('b16')" :suffix="$t('b16_u')" />
+                                    </a-input-group>
+                            </a-form-item>
+                        </a-form>
+                    </div>
+                </a-card>
+                <a-card :title="$t('referenceImage')">
+                    <div class="baseQueryParam">
+                        <a-form>
+                            <a-form-item style="margin-top: 50px; margin-bottom: 60px">
+                                <img width="400px" src="@/assets/img/5000/5001_1.png" />
+                            </a-form-item>
+                        </a-form>
+                    </div>
+                </a-card>
+            </a-col>
+            <a-col style="padding: 14px 6px" :xl="8" :lg="24" :md="24" :sm="24" :xs="24">
+
+            </a-col>
+            <a-col style="padding: 14px 6px; margin-right: 0px;" :xl="8" :lg="24" :md="24" :sm="24" :xs="24">
+            </a-col>
+        </a-row>
     </div>
 </template>
 
 <script>
+
+import { mapState } from 'vuex'
+
 export default {
+    components: {},
+    i18n: require('./i18n_8002'),
+    data() {
+        return {
+            b6: '10',
+            b7: '',
+            b8: '2660',
+            b9: '53',
+            b10: '7.4',
+            b11: '',
+            b12: '',
+            b13: '20',
+            b14: '200',
+            b15: '4',
+            b16: '50',
+        }
+    },
     methods: {
         backHome() {
             this.$router.push('/work')
+        },
+        exportQuantities() {
+            this.$message.warn(this.$t('exportQuantitiesNotOpen'))
+        },
+        exportComputeBook() {
+            this.$message.warn(this.$t('exportComputeBookNotOpen'))
         }
+        
     },
+    computed: {
+        ...mapState('setting', ['lang']),
+    }
 }
 </script>
+
+<style lang="less">
+.calc{
+    margin-bottom: 15px;
+}
+</style>
