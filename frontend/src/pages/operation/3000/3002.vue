114d1d89a8e06dbcdf6c52c2b9ee66f2a9d218ec
夂夂鱼 <dyzmj@qq.com>
2024-06-06 11:02:51 +0800

    完善3002部分计算逻辑

--- frontend/src/pages/operation/3000/3002.vue
+++ frontend/src/pages/operation/3000/3002.vue
@@ -6,10 +6,32 @@
                     :body-style="{ padding: 4 }">
                     <div class="baseQueryParam">
                         <a-form>
-                            <a-form-item :label="$t('c3_1')" style="margin-top: 10px" :labelCol="{ span: 6 }"
-                                :wrapperCol="{ span: 18 }">
-                                <a-input v-model="c3_1" :placeholder="$t('c3_1')"
-                                    :suffix="$t('c3_1_u')" value='120000' :disabled="false" />
+                            <a-form-item :label="$t('b3')" style="margin-top: 10px" :labelCol="{ span: 6 }" :wrapperCol="{ span: 18 }">
+                                <a-input v-model="b3" :placeholder="$t('b3')" :suffix="$t('b3_u')" :disabled="false" />
+                            </a-form-item>
+                            <a-form-item :label="$t('b4')" style="margin-top: 10px" :labelCol="{ span: 6 }" :wrapperCol="{ span: 18 }">
+                                <a-input v-model="b4" :placeholder="$t('b4')" :suffix="$t('b4_u')" :disabled="false" />
+                            </a-form-item>
+                            <a-form-item :label="$t('b5')" style="margin-top: 10px" :labelCol="{ span: 6 }" :wrapperCol="{ span: 18 }">
+                                <a-input v-model="b5" :placeholder="$t('b5')" :suffix="$t('b5_u')" :disabled="false" />
+                            </a-form-item>
+                            <a-form-item :label="$t('b6')" style="margin-top: 10px" :labelCol="{ span: 6 }" :wrapperCol="{ span: 18 }">
+                                <a-input v-model="b6" :placeholder="$t('b6')" :disabled="false" />
+                            </a-form-item>
+                            <a-form-item :label="$t('b7')" style="margin-top: 10px" :labelCol="{ span: 6 }" :wrapperCol="{ span: 18 }">
+                                <a-input v-model="b7" :placeholder="$t('b7')" :suffix="$t('b7_u')" :disabled="true" />
+                            </a-form-item>
+                            <a-form-item :label="$t('b8')" style="margin-top: 10px" :labelCol="{ span: 6 }" :wrapperCol="{ span: 18 }">
+                                <a-input v-model="b8" :placeholder="$t('b8')" :suffix="$t('b8_u')" :disabled="true" />
+                            </a-form-item>
+                            <a-form-item :label="$t('b9')" style="margin-top: 10px" :labelCol="{ span: 6 }" :wrapperCol="{ span: 18 }">
+                                <a-input v-model="b9" :placeholder="$t('b9')" :disabled="false" />
+                            </a-form-item>
+                            <a-form-item :label="$t('b10')" style="margin-top: 10px" :labelCol="{ span: 6 }" :wrapperCol="{ span: 18 }">
+                                <a-input v-model="b10" :placeholder="$t('b10')" :suffix="$t('b10_u')" :disabled="true" />
+                            </a-form-item>
+                            <a-form-item :label="$t('b11')" style="margin-top: 10px" :labelCol="{ span: 6 }" :wrapperCol="{ span: 18 }">
+                                <a-input v-model="b11" :placeholder="$t('b11')" :suffix="$t('b11_u')" :disabled="true" />
                             </a-form-item>
                         </a-form>
                     </div>
@@ -34,7 +56,13 @@ export default {
     i18n: require('./i18n_3002'),
     data() {
         return {
-            c3_1: '40000',
+            b3: '40000',
+            b4: '2',
+            b5: '15',
+            b6: '0.05',
+            b9: '4',
+            b16: '4',
+            b19: '0.5',
         }
     },
     methods: {
@@ -51,12 +79,18 @@ export default {
     },
     computed: {
         ...mapState('setting', ['lang']),
-        c3_2() {
-            return (parseFloat(this.c3_1) / 24).toFixed(0)
+        b7() {
+            return (parseFloat(this.b3) * (1 + parseFloat(this.b6)) / 2 / 86400).toFixed(2)
         },
-        c3_3() {
-            return (parseFloat(this.c3_1) / (24 * 60 * 60) ).toFixed(2)
+        b8() {
+            return ((parseFloat(this.b3) * (1 + parseFloat(this.b6)) / 2 / 86400) * parseFloat(this.b5) * 60).toFixed(2)
         },
+        b10() {
+            return (parseFloat(this.b8) / parseFloat(this.b9)).toFixed(2)
+        },
+        b11() {
+            return ((parseFloat(this.b3) * (1 + parseFloat(this.b6)) / 2 / 86400) / 0.12).toFixed(2)
+        }
     }
 }
 </script>
