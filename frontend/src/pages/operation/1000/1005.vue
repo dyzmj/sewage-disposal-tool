e32cebc365737271ceaea6386d646557a41c37c3
dongdongyu <dyzmj@qq.com>
2024-09-05 17:17:43 +0800

    增加1005计算书导出功能

--- frontend/src/pages/operation/1000/1005.vue
+++ frontend/src/pages/operation/1000/1005.vue
@@ -657,7 +657,7 @@
 
 <script>
 import { mapState } from "vuex";
-import { exportExcel3 } from "@/utils/exportUtil";
+import { exportExcel3, exportWord } from "@/utils/exportUtil";
 
 export default {
   components: {},
@@ -996,7 +996,12 @@ export default {
       return secondRowHeader;
     },
     exportComputeBook() {
-      this.$message.warn(this.$t("exportComputeBookNotOpen"));
+      const data = {
+        key1: this.b3,
+        key2: this.b9,
+        key3: this.b10,
+      };
+      exportWord("高锰酸钾计算书", "1005.docx", data, this);
     },
     getDisinfectiontank() {
       return "to do";
