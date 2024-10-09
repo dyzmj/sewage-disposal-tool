71c7ebac354d84fdec06cffb54552697b9ea2de3
dongdongyu <dyzmj@qq.com>
2024-10-09 14:44:48 +0800

    8001~8002增加与工作台设计水量关联功能

--- frontend/src/pages/operation/8000/8001.vue
+++ frontend/src/pages/operation/8000/8001.vue
@@ -604,7 +604,7 @@
 
 <script>
 import { mapState } from "vuex";
-import { exportExcel3, exportWord } from "@/utils/exportUtil";
+import { exportExcel3, exportWord, getValueFromLocalStorage } from "@/utils/exportUtil";
 
 export default {
   components: {},
@@ -870,6 +870,14 @@ export default {
     backHome() {
       this.$router.push("/work");
     },
+    initWaterData() {
+      const waterData = getValueFromLocalStorage("waterData")
+      if (waterData == null || waterData == ''){
+        this.b3 = 40000
+      }else{
+        this.b3 = waterData
+      }
+    },
     exportQuantities() {
       try{
         this.exportExcel();
@@ -1014,7 +1022,17 @@ export default {
       ).toFixed(2);
     },
   },
+  watch() {
+    this.initWaterData();
+  },
+  activated() {
+    this.initWaterData();
+  },
+  mounted() {
+    this.initWaterData();
+  },
   created() {
+    this.initWaterData();
     this.data1 = [
       {
         key: "1",
--- frontend/src/pages/operation/8000/8002.vue
+++ frontend/src/pages/operation/8000/8002.vue
@@ -954,7 +954,7 @@
 
 <script>
 import { mapState } from "vuex";
-import { exportExcel3, exportWord } from "@/utils/exportUtil";
+import { exportExcel3, exportWord, getValueFromLocalStorage } from "@/utils/exportUtil";
 
 export default {
   components: {},
@@ -1245,6 +1245,14 @@ export default {
     backHome() {
       this.$router.push("/work");
     },
+    initWaterData() {
+      const waterData = getValueFromLocalStorage("waterData")
+      if (waterData == null || waterData == ''){
+        this.b3 = 40000
+      }else{
+        this.b3 = waterData
+      }
+    },
     exportQuantities() {
       try{
         this.exportExcel();
@@ -1323,7 +1331,17 @@ export default {
   computed: {
     ...mapState("setting", ["lang"]),
   },
+  watch() {
+    this.initWaterData();
+  },
+  activated() {
+    this.initWaterData();
+  },
+  mounted() {
+    this.initWaterData();
+  },
   created() {
+    this.initWaterData();
     this.data1 = [
       {
         key: "1",
