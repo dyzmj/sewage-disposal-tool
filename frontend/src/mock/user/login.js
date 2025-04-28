b143789ced8cf56c4e1849aca491f7e524036777
dongdongyu <dyzmj@qq.com>
2025-04-28 13:59:38 +0800

    非管理员版本打包调整

--- frontend/src/mock/user/login.js
+++ frontend/src/mock/user/login.js
@@ -18,11 +18,7 @@ Mock.mock(`${process.env.VUE_APP_API_BASE_URL}/login`, 'post', ({body}) => {
     success = true
     result.data.permissions = [{id: 'queryForm', operation: ['add', 'edit']}]
     result.data.roles = [{id: 'admin', operation: ['add', 'edit', 'delete']}]
-  } else if (name === 'test' || password === '888888') {
-    success = true
-    result.data.permissions = [{id: 'queryForm', operation: ['add', 'edit']}]
-    result.data.roles = [{id: 'test', operation: ['add', 'edit', 'delete']}]
-  } else {
+  }else {
     success = false
   }
 
